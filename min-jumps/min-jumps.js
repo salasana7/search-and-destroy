2f9167f249fbc2329c43293543985e7d24420288
Camila Browne <camila.browne7@gmail.com>
2020-10-28 14:55:11 -0600

    until min jump comments'

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -5,6 +5,7 @@ const minJumps = arr => {
 //We might need to store in our DP table the shortest or least number of jumps to get to i (?)
 // for a base case of an array of length [1] the num of jumps is 0
 // DP[i] = 1 + the smallest of the other DPs that can still get us to this index (?)
+// the last element in our dp array is the answer 
 };
 
 module.exports = minJumps
